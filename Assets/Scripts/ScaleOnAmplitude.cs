0b6a800ea967fb25a340e6f760ac4094cf68a3e3
Elia <54859644+EliaAgents@users.noreply.github.com>
2023-06-19 15:45:08 +0100

    tempo attempt

--- Assets/Scripts/ScaleOnAmplitude.cs
+++ Assets/Scripts/ScaleOnAmplitude.cs
@@ -9,10 +9,20 @@ public class ScaleOnAmplitude : MonoBehaviour
     Material _material;
     public float _red, _green, _blue;
 
+    // so it follows bpm
+    public float _bpm;
+    private float _beatInterval;
+    private float _nextBeatTime;
+
     // Start is called before the first frame update
     void Start()
     {
         _material = GetComponent<MeshRenderer> ().materials [0];
+
+        // Calculate the time interval between beats based on the BPM
+        _beatInterval = 60f / _bpm;
+        _nextBeatTime = Time.time + _beatInterval; // Schedule the first beat
+    
     }
 
     // Update is called once per frame
@@ -27,6 +37,16 @@ public class ScaleOnAmplitude : MonoBehaviour
             transform.localScale = new Vector3((AudioPeer._AmplitudeBuffer[0] * _maxScale) + _startScale,(AudioPeer._AmplitudeBuffer[0] * _maxScale) + _startScale,(AudioPeer._AmplitudeBuffer[0] * _maxScale) + _startScale );
             Color _color = new Color (_red * AudioPeer._AmplitudeBuffer[0], _green * AudioPeer._AmplitudeBuffer[0], _blue * AudioPeer._AmplitudeBuffer[0]);
             _material.SetColor ("_EmissionColor", _color);
+        }
+
+        if (Time.time >= _nextBeatTime)
+        {
+            // Randomly increase or decrease the scale of the object
+            float randomScale = Random.Range(-0.1f, 0.1f);
+            transform.localScale += new Vector3(randomScale, randomScale, randomScale);
+
+            // Schedule the next beat
+            _nextBeatTime += _beatInterval;
         }
     }
 }
