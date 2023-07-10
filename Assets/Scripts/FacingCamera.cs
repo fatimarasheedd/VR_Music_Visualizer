23527c6ef5ed47310d867dc18c6802ebeb5e915b
Elia <54859644+EliaAgents@users.noreply.github.com>
2023-07-10 13:47:46 +0100

    controllers working

---
+++ Assets/Scripts/FacingCamera.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCamera : MonoBehaviour
+{
+    void LateUpdate() {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+}
