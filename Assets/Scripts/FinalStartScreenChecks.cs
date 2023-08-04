11620d1b910fed856bd8536253324e58aca58f79
fatimarasheedd <103149392+fatimarasheedd@users.noreply.github.com>
2023-08-04 12:26:01 +0100

    colour quiz 2d completed

--- Assets/Scripts/FinalStartScreenChecks.cs
+++ Assets/Scripts/FinalStartScreenChecks.cs
@@ -22,51 +22,118 @@ public class FinalStartScreenChecks : MonoBehaviour
     public Material MedHighArousal;
     public Material HighArousal;
 
+    // all the combined materials based on the grid
+    public Material HighALowV;
+    public Material HighAMedV;
+    public Material HighAHighV;
+    public Material MedALowV;
+    public Material MedAMedV;
+    public Material MedAHighV;
+    public Material LowALowV;
+    public Material LowAMedV;
+    public Material LowAHighV;
+
+    public static string HighALowVColour;
+    public static string HighAMedVColour;
+    public static string HighAHighVColour;
+    public static string MedALowVColour;
+    public static string MedAMedVColour;
+    public static string MedAHighVColour;
+    public static string LowALowVColour;
+    public static string LowAMedVColour;
+    public static string LowAHighVColour;
+
     public GameObject selectArousalValue;
+    public GameObject colourQuizPanel; 
 
     public void OnQuizCompleted()
     {
         // Quiz completed
-        lowValenceColor = "#" + ColorUtility.ToHtmlStringRGB(LowValence.color);
-        lowMediumValenceColor = "#" + ColorUtility.ToHtmlStringRGB(LowMedValence.color);
-        mediumHighValenceColor = "#" + ColorUtility.ToHtmlStringRGB(MedHighValence.color);
-        highValenceColor = "#" + ColorUtility.ToHtmlStringRGB(HighValence.color);
+
+        if (colourQuizPanel == null )
+        {
+            lowValenceColor = "#" + ColorUtility.ToHtmlStringRGB(LowValence.color);
+            lowMediumValenceColor = "#" + ColorUtility.ToHtmlStringRGB(LowMedValence.color);
+            mediumHighValenceColor = "#" + ColorUtility.ToHtmlStringRGB(MedHighValence.color);
+            highValenceColor = "#" + ColorUtility.ToHtmlStringRGB(HighValence.color);
         
-        if (selectArousalValue != null) {
-            lowArousalColor = "#" + ColorUtility.ToHtmlStringRGB(LowArousal.color);
-            lowMediumArousalColor = "#" + ColorUtility.ToHtmlStringRGB(LowMedArousal.color);
-            mediumHighArousalColor = "#" + ColorUtility.ToHtmlStringRGB(MedHighArousal.color);
-            highArousalColor = "#" + ColorUtility.ToHtmlStringRGB(HighArousal.color);
+            if (selectArousalValue != null) {
+                lowArousalColor = "#" + ColorUtility.ToHtmlStringRGB(LowArousal.color);
+                lowMediumArousalColor = "#" + ColorUtility.ToHtmlStringRGB(LowMedArousal.color);
+                mediumHighArousalColor = "#" + ColorUtility.ToHtmlStringRGB(MedHighArousal.color);
+                highArousalColor = "#" + ColorUtility.ToHtmlStringRGB(HighArousal.color);
+            } 
+        }
+
+        else {
+            HighALowVColour = "#" + ColorUtility.ToHtmlStringRGB(HighALowV.color);
+            MedALowVColour = "#" + ColorUtility.ToHtmlStringRGB(MedALowV.color);
+            LowALowVColour = "#" + ColorUtility.ToHtmlStringRGB(LowALowV.color);
+            HighAMedVColour = "#" + ColorUtility.ToHtmlStringRGB(HighAMedV.color);
+            MedAMedVColour = "#" + ColorUtility.ToHtmlStringRGB(MedAMedV.color);
+            LowAMedVColour = "#" + ColorUtility.ToHtmlStringRGB(LowAMedV.color);
+            HighAHighVColour = "#" + ColorUtility.ToHtmlStringRGB(HighAHighV.color);
+            MedAHighVColour = "#" + ColorUtility.ToHtmlStringRGB(MedAHighV.color);
+            LowAHighVColour = "#" + ColorUtility.ToHtmlStringRGB(LowAHighV.color);
+
         }
 
     }
 
     public void OnQuizNotCompleted(){
 
-        // Set the hex colors for different valence and arousal levels
-        lowValenceColor = "#0D026E"; 
-        lowMediumValenceColor = "#9DCAEB";
-        mediumHighValenceColor = "#FFFF00"; 
-        highValenceColor = "#FF00FF"; 
-        
-        if (selectArousalValue != null){
-            lowArousalColor = "#ADD8E6"; 
-            lowMediumArousalColor = "#F6C6BD"; 
-            mediumHighArousalColor = "#FCDA3F"; 
-            highArousalColor = "#F22400";
+        if (colourQuizPanel == null )
+        {
+            // Set the hex colors for different valence and arousal levels
+            lowValenceColor = "#0D026E"; 
+            lowMediumValenceColor = "#9DCAEB";
+            mediumHighValenceColor = "#FFFF00"; 
+            highValenceColor = "#FF00FF"; 
+            
+            if (selectArousalValue != null){
+                lowArousalColor = "#ADD8E6"; 
+                lowMediumArousalColor = "#F6C6BD"; 
+                mediumHighArousalColor = "#FCDA3F"; 
+                highArousalColor = "#F22400";
+            }
+        }
+        else {
+            // red ish colour
+            HighALowVColour = "#FF1100";
+            // dark purple ish 
+            MedALowVColour = "#3D0087";
+            // black - dark brown - grey 
+            LowALowVColour = "#4A4A4A";
+            // light pink 
+            HighAMedVColour = "#FFA6F9";
+            // yellow - green
+            MedAMedVColour = "#89AD4E";
+            // dark green
+            LowAMedVColour = "#003006";
+            // yellow 
+            HighAHighVColour = "#DCE305";
+            // bright blue
+            MedAHighVColour = "#29DFFF";
+            // white 
+            LowAHighVColour = "#F0F0F0";
         }
         
 
     }
     public void StartGame(){
         Debug.Log("Game starting!");
-
-        if (selectArousalValue != null){
+        if (colourQuizPanel == null ){
+            if (selectArousalValue != null){
             UnityEngine.SceneManagement.SceneManager.LoadScene("2. VR_Room_Official");
+            }
+            else {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("2. VR_Room_Official3");
+            }
         }
         else {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("2. VR_Room_Official3");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("2. VR_Room_Official2");
         }
+        
     }
     
 }
---
+++ Assets/Scripts/SetupVariableExperiment2.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupVariableExperiment2 : MonoBehaviour
+{
+    void Awake()
+    {
+        string selectedTrackName = TrackPrefabController.selectedSong;
+        
+        // Load the selected audio clip based on the selectedTrackName
+        AudioClip selectedAudioClip = Resources.Load<AudioClip>(selectedTrackName);
+
+        // Check if the audio clip is valid
+        if (selectedAudioClip != null)
+        {
+            // Find the existing audio source component on the audio game object
+            AudioSource audioSource = GameObject.Find("Audio").GetComponent<AudioSource>();
+
+            // Assign the loaded audio clip to the existing audio source component
+            audioSource.clip = selectedAudioClip;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Selected audio clip is null. Make sure the audio clip exists in the 'Audio' folder.");
+        }
+
+        // Get the reference to the Lights game object
+        GameObject lightsGameObject = GameObject.Find("Lights");
+
+        // Get the ValenceSpotLight and ArousalSpotLight child game objects
+        GameObject valenceSpotLight = lightsGameObject.transform.Find("ValenceSpotLight").gameObject;
+        GameObject arousalSpotLight = lightsGameObject.transform.Find("ArousalSpotLight").gameObject;
+        GameObject valenceSpotLight2 = lightsGameObject.transform.Find("ValenceSpotLight2").gameObject;
+        GameObject arousalSpotLight2 = lightsGameObject.transform.Find("ArousalSpotLight2").gameObject;
+
+
+        // low A low V
+        if (TrackPrefabController.selectedArousal <= 0.33f && TrackPrefabController.selectedValence <= 0.33f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.LowALowVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // low A med V
+        else if (TrackPrefabController.selectedArousal <= 0.33f && TrackPrefabController.selectedValence <= 0.66f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.LowAMedVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // low A high  V
+        else if (TrackPrefabController.selectedArousal <= 0.33f && TrackPrefabController.selectedValence <= 1.00f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.LowAHighVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // med A low V
+        else if (TrackPrefabController.selectedArousal <= 0.66f && TrackPrefabController.selectedValence <= 0.33f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.MedALowVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // med A med V
+        else if (TrackPrefabController.selectedArousal <= 0.66f && TrackPrefabController.selectedValence <= 0.66f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.MedAMedVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // med A high  V
+        else if (TrackPrefabController.selectedArousal <= 0.66f && TrackPrefabController.selectedValence <= 1.00f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.MedAHighVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // high A low V
+        else if (TrackPrefabController.selectedArousal <= 1.00f && TrackPrefabController.selectedValence <= 0.33f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.HighALowVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // high A med V
+        else if (TrackPrefabController.selectedArousal <= 1.00f && TrackPrefabController.selectedValence <= 0.66f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.HighAMedVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+        // high A high  V
+        else if (TrackPrefabController.selectedArousal <= 1.00f && TrackPrefabController.selectedValence <= 1.00f){
+            ColorUtility.TryParseHtmlString(FinalStartScreenChecks.HighAHighVColour, out Color color);
+            arousalSpotLight.GetComponent<Light>().color = color;
+            arousalSpotLight2.GetComponent<Light>().color = color;
+            valenceSpotLight.GetComponent<Light>().color = color;
+            valenceSpotLight2.GetComponent<Light>().color = color;
+        }
+
+    }
+}
